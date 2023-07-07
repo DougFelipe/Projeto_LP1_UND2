170c6415f408c6f3809086811dd665f4786b811c
Douglas Felipe <117777160+DougFelipe@users.noreply.github.com>
2023-07-07 16:37:35 -0300

    Adiciona algoritmo selection sort

--- include/Ordenacao.hpp
+++ include/Ordenacao.hpp
@@ -6,13 +6,16 @@
 #ifndef ORDENACAO_HPP
 #define ORDENACAO_HPP
 
+#include <algorithm>
 #include "Livro.h"
+#include "Autor.h"
+#include "Editora.h"
+#include "Leitor.h"
 
 /**
  * @brief Função que ordena um array de livros por título.
  * 
- * Algoritmo Bubble Sort para ordenar
- * um array de Livros em ordem alfabética pelo título.
+ * Algoritmo Bubble Sort para ordenar um array de Livros em ordem alfabética pelo título.
  *
  * @param livros O array de Livro a ser ordenado.
  * @param n A quantidade de livros no array.
@@ -37,8 +40,7 @@ void ordenarPorTitulo(Livro* livros, int n) //bubble sort
 /**
  * @brief Função que ordena um array de livros por ano de publicação.
  * 
- * Algoritmo Bubble Sort para ordenar
- * um array de Livros pelo ano de publicação.
+ * Algoritmo Bubble Sort para ordenar um array de Livros pelo ano de publicação.
  *
  * @param livros O array de Livro a ser ordenado.
  * @param n A quantidade de livros no array.
@@ -57,6 +59,32 @@ void ordenarPorAnoPublicacao(Livro* livros, int n) //bubble sort
                 livros[j+1] = temp;
             }
         }
+    }
+}
+
+/**
+ * @brief Ordenação por selection sort.
+ * 
+ * Ordena um array de elementos do tipo T de acordo com o nome.
+ * 
+ * @tparam T O tipo dos elementos do array.
+ * 
+ * @param arr Referência para o array a ser ordenado.
+ * @param n O número de elementos no array.
+ */
+
+template<typename T>
+void selectionSort(T& arr, int n) 
+{
+    int min_idx;
+  
+    for (int i = 0; i < n-1; i++) 
+    {
+        min_idx = i;
+        for (int j = i+1; j < n; j++)
+            if (arr[j]->getNome() < arr[min_idx]->getNome())
+                min_idx = j;
+        std::swap(arr[min_idx], arr[i]);
     }
 }
 
