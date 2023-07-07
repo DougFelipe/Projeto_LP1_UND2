51ec10040584cdd200ae2e3ee6a19b8897fce0b3
Douglas Felipe <117777160+DougFelipe@users.noreply.github.com>
2023-07-06 22:21:10 -0300

    Adicionando códigos para bubble sort

---
+++ include/OrdenacaoLivros.h
@@ -0,0 +1,9 @@
+#ifndef ORDENACAOLIVROS_H
+#define ORDENACAOLIVROS_H
+
+#include "Livro.h"
+
+void ordenarPorTitulo(Livro* livros, int n);
+void ordenarPorAnoPublicacao(Livro* livros, int n);
+
+#endif
